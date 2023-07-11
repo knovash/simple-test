52404bc84755fc2e77192a43de11bf26ecacb094
Konstantin <novash.ki@gmail.com>
2023-07-12 01:12:37 +0300

    local

--- src/test/java/org/itacademy/homework9selenide/BaseTest.java
+++ src/test/java/org/itacademy/homework9selenide/BaseTest.java
@@ -3,19 +3,31 @@ package org.itacademy.homework9selenide;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Browsers.CHROME;
 
 @Log4j2
 public class BaseTest {
 
+        public static WebDriver driver;
+
     @BeforeClass
     public static void beforeClass() {
         log.info("BEFORE CLASS");
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = CHROME;
+
+        System.setProperty("webdriver.chrome.driver", "/home/konstantin/Downloads/chromedriver_linux64_114/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = CHROME;
 //        System.setProperty("webdriver.chrome.driver", "/home/konstantin/Downloads/chromedriver_linux64 114/chromedriver");
 //        System.setProperty("selenide.browser", "Chrome");
     }
