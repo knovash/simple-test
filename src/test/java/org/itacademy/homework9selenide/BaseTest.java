8192ac02a8da5252bd8a3e39dc211f9334447983
Konstantin <novash.ki@gmail.com>
2023-07-12 00:39:44 +0300

    local

--- src/test/java/org/itacademy/homework9selenide/BaseTest.java
+++ src/test/java/org/itacademy/homework9selenide/BaseTest.java
@@ -1,32 +1,23 @@
 package org.itacademy.homework9selenide;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import io.qameta.allure.selenide.LogType;
 import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.logging.Level;
-
-//import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Browsers.CHROME;
 
 @Log4j2
 public class BaseTest {
 
     @BeforeClass
-    public static void startLocal() {
-        log.info("START TYPE LOCAL");
+    public static void beforeClass() {
+        log.info("BEFORE CLASS");
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = CHROME;
 //        System.setProperty("webdriver.chrome.driver", "/home/konstantin/Downloads/chromedriver_linux64 114/chromedriver");
 //        System.setProperty("selenide.browser", "Chrome");
-//        SelenideLogger.addListener("allure", new AllureSelenide()
-//                .screenshots(true)
-//                .savePageSource(true)
-//                .enableLogs(LogType.BROWSER, Level.ALL));
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.browser = CHROME;
-//        Configuration.browserCapabilities.
     }
 
     @BeforeMethod
