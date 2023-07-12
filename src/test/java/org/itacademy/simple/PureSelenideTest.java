72b3dddd80f9156e74a7ecbfc765659827da2880
Konstantin <novash.ki@gmail.com>
2023-07-12 10:08:01 +0300

    upd

--- src/test/java/org/itacademy/simple/PureSelenideTest.java
+++ src/test/java/org/itacademy/simple/PureSelenideTest.java
@@ -1,13 +1,10 @@
-package org.itacademy.homework9selenide;
+package org.itacademy.simple;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,38 +18,12 @@ public class PureSelenideTest {
     @Test()
     public void pureSelenideTest() {
         log.info("BEFORE CLASS");
-//        System.setProperty("webdriver.chrome.driver", "/home/konstantin/Downloads/chromedriver_linux64_114/chromedriver");
-//
-//        var chromeOptions = new ChromeOptions();
-//        chromeOptions.addArguments("--no-sandbox"); // with this it works; without, doesn't
-////        var driver = new ChromeDriver(chromeOptions);
-//
-//        WebDriver driver = new ChromeDriver(chromeOptions);
-//
-//        driver.get("https://donerking.by/");
-
-
-
-//        Configuration.browserSize = "1920x1080";
         Configuration.browser = CHROME;
-//        Configuration.browserCapabilities.setCapability("");
-//        System.setProperty("webdriver.chrome.driver", "/home/konstantin/Downloads/chromedriver_linux64 114/chromedriver");
-//        System.setProperty("selenide.browser", "Chrome");
-
         Selenide.open("https://donerking.by/");
 
 
-
-
-
-
-
-
-
-
-
-        String item = "бургер";
         log.info("TEST SEARCH");
+        String item = "бургер";
         log.info("DATA " + item);
         log.info("CLICK SEARCH BUTTON");
         $(By.xpath("//a[@class='main-header__search-block']//*[name()='svg']")).click();
